1038303b8b12ef9983fbecf39925b56ea8f5dc43
STmihan <mih.dunaev@gmail.com>
2021-10-26 22:13:00 +0300

    Добавил адаптивную установку сцены

---
+++ Assets/Scripts/Scene/SceneSetter.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+using Random = UnityEngine.Random;
+
+namespace Scene
+{
+    public struct SceneBorders
+    {
+        public Vector2 TopBorder, RightBorder;
+
+        public SceneBorders(Vector2 topBorder, Vector2 rightBorder)
+        {
+            TopBorder = topBorder;
+            RightBorder = rightBorder;
+        }
+
+        public void InflateBorders(float arg)
+        {
+            TopBorder += new Vector2(0, arg);
+            RightBorder += new Vector2(arg, 0);
+        }
+    }
+    
+    public class SceneSetter : MonoBehaviour
+    {
+        [SerializeField] private Assets.Assets _assets;
+        [Space]
+        [SerializeField] private Transform _backgroundTiles;
+        
+        private BoxCollider2D _collider;
+        
+        private void Start()
+        {
+            _collider = GetComponent<BoxCollider2D>();
+            SetScene(_collider);
+            SetBackground(_assets, _collider);
+        }
+
+        private void SetScene(BoxCollider2D collider)
+        {
+            Camera cum = Camera.allCameras[0];
+            var sceneBorders = new SceneBorders(
+                 cum.ScreenToWorldPoint(new Vector2(Screen.width/2f, Screen.height)),
+                 cum.ScreenToWorldPoint(new Vector2(Screen.width, Screen.height/2f))
+            );
+            sceneBorders.InflateBorders(.1f); // Магическое число для раздувания границ
+            collider.size = new Vector2(sceneBorders.RightBorder.x * 2, sceneBorders.TopBorder.y * 2);
+        }
+
+        private void SetBackground(Assets.Assets assets, BoxCollider2D collider)
+        {
+            Sprite bg = assets.backgrounds[Random.Range(0, assets.backgrounds.Length)];
+
+            Vector2Int sceneBordersX = new Vector2Int(
+                Mathf.FloorToInt(collider.bounds.min.x),
+                Mathf.CeilToInt(collider.bounds.max.x)
+                );
+            Vector2Int sceneBordersY = new Vector2Int(
+                Mathf.FloorToInt(collider.bounds.min.y),
+                Mathf.CeilToInt(collider.bounds.max.y)
+            );
+            
+            for (int x = sceneBordersX.x; x < sceneBordersX.y; x++)
+            for (int y = sceneBordersY.x; y < sceneBordersY.y; y++)
+            {
+                var go = new GameObject("tile");
+                var spriteRenderer = go.AddComponent<SpriteRenderer>();
+                spriteRenderer.sprite = bg;
+                go.transform.position = new Vector2(x, y);
+                go.transform.SetParent(_backgroundTiles);
+            }
+        }
+    }
+}
